d129dd92f41bd03f27aa7af971ddac98c0f7147f
Connor Moore <rconmoore@gmail.com>
2025-05-28 12:43:12 -0400

    repo setup

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-$\textbf{pitch-ml}$
+`pitch-ml`$\textbf{: Optimizing Pitcher Health \& Performance}$
 
 Ball tracking, biomechanics, and machine learning algorithms for optimizing pitcher health and performance. 
 
@@ -6,6 +6,10 @@ $\textbf{1. Repository Overview}$
 
 Coming soon.
 
-$\textbf{A.1 Publications}$
+$\textbf{A.1 Environment Setup}$
+
+All code is configured to be run in a conda virtual environment (details in `pitch_ml.yml`) from Python `3.11.10`, which has all OpenSim API dependencies installed. To activate the environment, call `conda activate pitch_ml`. 
+
+$\textbf{A.2 Publications}$
 
 Any publications associated with this repository will be linked here.
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pitch_ml",
+    version="0.0.1",
+    packages=find_packages(where="packages"),
+    package_dir={"": "packages"},
+    python_requires=">=3.8",
+    install_requires=[
+        'pandas>=2.0',
+        'torch>=2.2.2',
+        'dropbox>=12.0.2',
+        'scipy>=1.14.0',
+        'matplotlib>=3.4.0',
+    ],
+    author='Connor Moore',
+    author_email='rcm8@clemson.edu',
+    description='Pitch ML: Packages for optimizing pitcher health and performance.',
+    url='https://github.com/humanrithm/pitch-ml',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: Other/Proprietary License',
+        'Operating System :: OS Independent',
+    ],
+)
